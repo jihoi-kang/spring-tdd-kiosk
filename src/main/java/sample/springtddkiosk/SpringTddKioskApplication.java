0caa01ca2430d46f9f0888b17732b4006dd606c3
Jihoi-Kang <jihoi.kang@gmail.com>
2024-02-27 17:38:55 +0900

    init

---
+++ src/main/java/sample/springtddkiosk/SpringTddKioskApplication.java
@@ -0,0 +1,13 @@
+package sample.springtddkiosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTddKioskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTddKioskApplication.class, args);
+	}
+
+}
